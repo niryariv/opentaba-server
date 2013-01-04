efb05a2e66695834a4b9a070aed7b19784abd4b5
Nir Yariv <niryariv@gmail.com>
2013-01-04 15:54:06 +0200

    added pgsql

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def hello():
