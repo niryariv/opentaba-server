13ce183103172fe5f7ec2c5753d9809f83a38cc4
Nir Yariv <niryariv@gmail.com>
2013-01-08 17:03:58 +0200

    work with cache files when running locall

--- scraper.py
+++ scraper.py
@@ -1,27 +1,34 @@
 from app import *
 from tools.scrapelib import *
 import md5
-
+import os
 
 # accepts a gush object, scrapes date from gush URL and saves it into the plans collection
 def scrape_gush(gush):
 
 	gush_id = gush['gush_id']
+
 	print "checking gush %s" % gush_id
 
-	# check if the html matches a pre-read html
-	html = get_gush_html(gush_id)
-
-	# save in cache
 	if RUNNING_LOCAL:
-		f = open("filecache/%s.html" % gush_id, "wb")
-		f.write(html.encode('utf-8'))
-		f.close()
+		local_cache = "filecache/%s.html" % gush_id
+		if os.path.exists(local_cache):
+			print "reading local file %s" % local_cache
+			html = open(local_cache, 'r').read()
+			# return html
+		else:
+			html = get_gush_html(gush_id)
+			open(local_cache, 'wb').write(html.encode('utf-8'))
 
-	
-	html_hash = md5.new(html.encode('utf-8')).hexdigest()
-	print "html_hash: %s" % html_hash
+		html_hash = md5.new(html).hexdigest()
 
+	else:
+		html = get_gush_html(gush_id)
+		html_hash = md5.new(html.encode('utf-8')).hexdigest()
+
+	# check if the html matches a pre-read html
+	# html_hash = md5.new(html.encode('utf-8')).hexdigest()
+	# print "html_hash: %s" % html_hash
 	if gush["html_hash"] == html_hash:
 		print "duplicate HTML - returning"
 		return True
