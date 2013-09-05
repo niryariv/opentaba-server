52c006d34a92dab36fcd48ede5e00067241ac43d
alonisser <alonisser@gmail.com>
2013-09-05 23:59:54 +0300

    removing the previously commented out tests

--- Tests/functional_tests/test_return_json.py
+++ Tests/functional_tests/test_return_json.py
@@ -38,23 +38,6 @@ def test_api_get_gush():
     eq_(j.keys(), ['_id','gush_id', 'last_checked_at', 'html_hash'])
 
 
-#class Json_test(TestCase):
-#
-#    def create_app(self):
-#        app = Flask(__name__)
-#        app.config['TESTING'] = True
-#        return app
-#
-#    def test_gushim(self):
-#        response = self.client.get("/gushim/")
-#        self.assert200(response)
-#
-#    def test_get_gush(self):
-#        response = self.client.get("/gush/30035/")
-#        print(response)
-#        eq_(response.json, 
-#{"_id": {"$oid": "50ec3aa7e16ddfd10b275d01"}, "gush_id": "30035", "last_checked_at": {"$date": 1358175962596}, "html_hash": "041363772c628becdd339a16d5f7cf2a"})
-#
 #    def test_get_block(self):
 #        response = self.client.get("/gush/30540/plans")
 #        data = response.json  # maybe should be without the () to be a method
