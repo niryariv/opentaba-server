d1492cc3260076938e82adb12aa3c5d72e5a177a
alonisser <alonisser@gmail.com>
2013-09-06 13:34:38 +0300

    implented a api plans test and api wakeup test

--- Tests/functional_tests/test_return_json.py
+++ Tests/functional_tests/test_return_json.py
@@ -28,27 +28,53 @@ def test_api_gushim():
     print(dir(response))
     j = json.loads(response.data)
     eq_(len(j), 644) #the correct number
-    eq_(response.status_code,200)
+    eq_(response.status_code, 200)
     eq_(response.mimetype, 'application/json')
 
 @with_setup(setup, teardown)
 def test_api_get_gush():
     response = testapp.get('/gush/30035')
     j = json.loads(response.data)
+    eq_(response.status_code, 200)
+    eq_(response.mimetype, 'application/json')
     eq_(j.keys(), ['_id','gush_id', 'last_checked_at', 'html_hash'])
+    eq_(j['gush_id'], '30035')
+
+@with_setup(setup, teardown)
+def test_api_get_plan():
+    response = testapp.get('/gush/30035/plans')
+    j = json.loads(response.data)
+    eq_(response.status_code, 200)
+    eq_(response.mimetype, 'application/json')
+    eq_(len(j), 47) # The correct number
+    sample = j[0]
+    eq_(sample.keys(),[u'status',
+                        u'tasrit_link',
+                        u'gush_id',
+                        u'area',
+                        u'essence',
+                        u'files_link',
+                        u'nispahim_link',
+                        u'number',
+                        u'month',
+                        u'takanon_link',
+                        u'govmap_link',
+                        u'year',
+                        u'details_link',
+                        u'_id',
+                        u'day'])
+    eq_(sample['status'], u"פרסום בעיתונות להפקדה ")
+    eq_(sample['gush_id'], '30035')
+    eq_(sample['details_link'], u'taba4.asp?num=249&rec=79&gis=false')
+    eq_(sample['takanon_link'], [])
+    eq_(sample['essence'], u"השלמת קומה והרחבות דיור")
 
 
-#    def test_get_block(self):
-#        response = self.client.get("/gush/30540/plans")
-#        data = response.json  # maybe should be without the () to be a method
-#        print(len(data))
-#        eq_(len(data),3)
-#        d1 = data[0]
-#        eq_(len(d1['tasrit_link']),2)
-#        eq_(d1['tasrit_link'][0],'http://mmi.gov.il//IturTabotData/tabot/jerus/1011879_K.pdf')
-#        eq_(d1['gush_id'],"30540")
-#        eq_(d1['area'],u"ירושלים")
-#        eq_(d1['takanon_link'][0],'http://mmi.gov.il//IturTabotData/takanonim/jerus/1011879.pdf')
-#
-#
-#
+@with_setup(setup, teardown)
+def test_api_wakeup():
+    response = testapp.get('/wakeup')
+    j = json.loads(response.data)
+    eq_(response.status_code, 200)
+    eq_(response.mimetype, 'application/json')
+    eq_(j, {"morning": "good"})
+
