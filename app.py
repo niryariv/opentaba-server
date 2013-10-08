181382b25851ebb96874b5d388030fb773d1ef88
Shahar Evron <shahar.evron@gmail.com>
2013-10-08 11:17:19 +0300

    Slight refactor of feed route, adding feed for one or more Gushim (niryariv/opentaba-server#20)

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ else:  # work locally
     RUNNING_LOCAL = True
     app.debug = True  # since we're local, keep debug on
 
-
 #### Helpers ####
+
 
 def _to_json(mongo_obj):
     """
@@ -48,6 +48,37 @@ def _resp(data):
     r.headers['Content-Type'] = "application/json; charset=utf-8"
     return r
 
+
+def _plans_query_to_atom_feed(request, query={}):
+    """
+    Create an atom feed of plans fetched from the DB based on an optional query
+    """
+    plans = db.plans.find(query, limit=2000).sort(
+        [("year", pymongo.DESCENDING), ("month", pymongo.DESCENDING), ("day", pymongo.DESCENDING)])
+    blacklist = db.blacklist.find_one()['blacklist']
+    plans_clean = [p for p in list(plans) if p['number'] not in blacklist]
+
+    feed = AtomFeed("OpenTABA", feed_url=request.url, url=request.url_root)
+
+    for p in plans_clean:
+        url = 'http://mmi.gov.il/IturTabot/taba4.asp?' + url_encode({'kod': 3000, 'MsTochnit': p['number']},
+                                                                    charset='windows-1255')
+        try:
+            feed.add(
+                title=p['essence'],
+                content=p['status'] + p['number'],
+                content_type='html',
+                author="OpenTABA.info",
+                id=url + '&status=' + p['status'],
+                # this is a unique ID (not real URL) so adding status to ensure uniqueness in TBA stages
+                url=url,
+                updated=datetime.date(p['year'], p['month'], p['day'])
+            )
+        except ValueError:
+            # happens when title is empty, for now skipping
+            pass
+
+    return feed
 
 #### ROUTES ####
 
@@ -92,34 +123,21 @@ def get_plans(gush_id):
 
 @app.route('/feed.atom')
 def atom_feed():
-    feed = AtomFeed("OpenTABA", feed_url=request.url, url=request.url_root)
+    return _plans_query_to_atom_feed(request).get_response()
 
-    plans = db.plans.find(limit=2000).sort(
-        [("year", pymongo.DESCENDING), ("month", pymongo.DESCENDING), ("day", pymongo.DESCENDING)])
 
-    blacklist = db.blacklist.find_one()['blacklist']
-    plans_clean = [p for p in list(plans) if p['number'] not in blacklist]
-
-    for p in plans_clean:
-        url = 'http://mmi.gov.il/IturTabot/taba4.asp?' + url_encode({'kod': 3000, 'MsTochnit': p['number']},
-                                                                    charset='windows-1255')
-
-        content = p['status'] + p['number']
-        title = p['essence']
-        if not title:
-            title = content
-        feed.add(
-            title=title,
-            content=content,
-            content_type='html',
-            author="OpenTABA.info",
-            id=url + '&status=' + p['status'],
-            # this is a unique ID (not real URL) so adding status to ensure uniqueness in TBA stages
-            url=url,
-            updated=datetime.date(p['year'], p['month'], p['day'])
-        )
-
-    return feed.get_response()
+@app.route('/feed/gush/<gushim>.atom')
+def atom_feed_gush(gushim):
+    """
+    Create a feed for one or more gush IDs.
+    The URL format for multiple gushim is something like /feed/gush/12340|12350|12360
+    """
+    gushim = gushim.split('|')
+    if len(gushim) > 1:
+        query = {"gush_id": {"$in": gushim}}
+    else:
+        query = {"gush_id": gushim[0]}
+    return _plans_query_to_atom_feed(request, query).get_response()
 
 
 # TODO add some text on the project
@@ -134,7 +152,7 @@ def hello():
 @app.route('/wakeup')
 def wakeup():
     """
-    wake up heroku dyno from idle. perhaps can if >1 dynos
+    wake up Heroku dyno from idle. perhaps can if >1 dynos
     used as endpoint for a "wakeup" request when the client inits
     """
     return _resp({'morning': 'good'})
