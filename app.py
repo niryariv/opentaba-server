09df9a57fbe021fed611e97a1df2ebf2d668d86f
alonisser <alonisser@gmail.com>
2013-10-08 16:22:59 +0300

    added gush feedi, pep8 fixes

--- app.py
+++ app.py
@@ -18,11 +18,11 @@ app = Flask(__name__)
 
 MONGO_URL = os.environ.get('MONGOHQ_URL')
 
-if MONGO_URL:	# on Heroku, get a connection
-    m_conn = pymongo.Connection(MONGO_URL)   
+if MONGO_URL:   # on Heroku, get a connection
+    m_conn = pymongo.Connection(MONGO_URL)
     db = m_conn[urlparse(MONGO_URL).path[1:]]
     RUNNING_LOCAL = False
-else:			# work locally
+else:           # work locally
     try:
         m_conn = pymongo.Connection('localhost', 27017)
     except ConnectionFailure:
@@ -37,14 +37,14 @@ else:			# work locally
 
 # convert dictionary to JSON. json_util.default adds automatic mongoDB result support
 def _to_json(mongo_obj):
-	return json.dumps(mongo_obj, ensure_ascii=False, default=json_util.default)
+    return json.dumps(mongo_obj, ensure_ascii=False, default=json_util.default)
 
 
 def _resp(data):
-	r = make_response(_to_json(data))
-	r.headers['Access-Control-Allow-Origin'] = "*"
-	r.headers['Content-Type'] = "application/json; charset=utf-8"
-	return r
+    r = make_response(_to_json(data))
+    r.headers['Access-Control-Allow-Origin'] = "*"
+    r.headers['Content-Type'] = "application/json; charset=utf-8"
+    return r
 
 
 #### ROUTES ####
@@ -52,77 +52,111 @@ def _resp(data):
 # get gush_id metadata
 @app.route('/gushim')
 def get_gushim():
-	gushim = db.gushim.find(fields={'gush_id': True, '_id' : False})
-	return _resp(list(gushim))
+    gushim = db.gushim.find(fields={'gush_id': True, '_id': False})
+    return _resp(list(gushim))
 
 
 # get gush_id metadata
 @app.route('/gush/<gush_id>')
 def get_gush(gush_id):
-	gush = db.gushim.find_one({"gush_id" : gush_id})
-	if gush is None:
-		abort(404)
-	return _resp(gush)
+    gush = db.gushim.find_one({"gush_id": gush_id})
+    if gush is None:
+        abort(404)
+    return _resp(gush)
 
 
 # get plans from gush_id
 @app.route('/gush/<gush_id>/plans')
 def get_plans(gush_id):
-	if db.gushim.find_one({"gush_id" : gush_id}) is None:
-		abort(404)
+    if db.gushim.find_one({"gush_id": gush_id}) is None:
+        abort(404)
 
-	plans = db.plans.find({"gush_id" : gush_id}).sort([("year", pymongo.DESCENDING), ("month", pymongo.DESCENDING), ("day", pymongo.DESCENDING)])
-	
-	# eliminate plans which appear in >99 blocks - cover for MMI's database bugs
-	blacklist = db.blacklist.find_one()['blacklist']
+    plans = db.plans.find({"gush_id": gush_id}).sort([("year", pymongo.DESCENDING),
+                                                      ("month", pymongo.DESCENDING),
+                                                      ("day", pymongo.DESCENDING)])
 
-	plans_clean = [p for p in list(plans) if p['number'] not in blacklist]
+    # eliminate plans which appear in >99 blocks - cover for MMI's database bugs
+    blacklist = db.blacklist.find_one()['blacklist']
 
-	return _resp(plans_clean)
+    plans_clean = [p for p in list(plans) if p['number'] not in blacklist]
+
+    return _resp(plans_clean)
 
 
 @app.route('/feed')
 def atom_feed():
-	feed = AtomFeed("OpenTABA", feed_url=request.url, url=request.url_root)
+    feed = AtomFeed("OpenTABA", feed_url=request.url, url=request.url_root)
 
-	plans = db.plans.find(limit=2000).sort([("year", pymongo.DESCENDING), ("month", pymongo.DESCENDING), ("day", pymongo.DESCENDING)])
+    plans = db.plans.find(limit=2000).sort([("year", pymongo.DESCENDING), ("month", pymongo.DESCENDING), ("day", pymongo.DESCENDING)])
 
-	blacklist = db.blacklist.find_one()['blacklist']
-	plans_clean = [p for p in list(plans) if p['number'] not in blacklist]
+    blacklist = db.blacklist.find_one()['blacklist']
+    plans_clean = [p for p in list(plans) if p['number'] not in blacklist]
 
-	for p in plans_clean:
-		url = 'http://mmi.gov.il/IturTabot/taba4.asp?' + url_encode({'kod' : 3000, 'MsTochnit' : p['number']}, charset='windows-1255')
+    for p in plans_clean:
+        url = 'http://mmi.gov.il/IturTabot/taba4.asp?' + url_encode({'kod' : 3000, 'MsTochnit' : p['number']}, charset='windows-1255')
 
-		feed.add(
-			title 	= p['essence'],
-			content	=p['status'] + p['number'],
-			content_type = 'html',
-			author = "OpenTABA.info",
-			id	= url + '&status=' + p['status'], # this is a unique ID (not real URL) so adding status to ensure uniqueness in TBA stages
-			url = url, 
-			updated=datetime.date(p['year'], p['month'], p['day'])
-		)
+        feed.add(
+            title   = p['essence'],
+            content =p['status'] + p['number'],
+            content_type = 'html',
+            author = "OpenTABA.info",
+            id  = url + '&status=' + p['status'], # this is a unique ID (not real URL) so adding status to ensure uniqueness in TBA stages
+            url = url,
+            updated=datetime.date(p['year'], p['month'], p['day'])
+        )
 
-	return feed.get_response()
+    return feed.get_response()
 
 
+@app.route('/<gush_id>/feed')
+def atom_city_feed(gush_id):
+    feed = AtomFeed("OpenTABA_gush_%s" % gush_id,
+                    feed_url=request.url,
+                    url=request.url_root)
+    if db.gushim.find_one({"gush_id": gush_id}) is None:
+        abort(404)
+    plans = db.plans.find({"gush_id": gush_id}).sort([("year", pymongo.DESCENDING), ("month", pymongo.DESCENDING), ("day", pymongo.DESCENDING)])
+
+    # eliminate plans which appear in >99 blocks - cover for MMI's database bugs
+    blacklist = db.blacklist.find_one()['blacklist']
+
+    plans_clean = [p for p in list(plans) if p['number'] not in blacklist]
+
+    for p in plans_clean:
+        url = 'http://mmi.gov.il/IturTabot/taba4.asp?' + url_encode({'kod': 3000,
+                                                                     'MsTochnit': p['number']},
+                                                                    charset='windows-1255')
+
+        feed.add(
+            title=p['essence'],
+            content=p['status'] + p['number'],
+            content_type='html',
+            author="OpenTABA.info",
+            id =url + '&status=' + p['status'],  # this is a unique ID (not real URL) so adding status to ensure uniqueness in TBA stages
+            url=url,
+            updated=datetime.date(p['year'], p['month'], p['day'])
+        )
+
+    return feed.get_response()
 
 # TODO add some text on the project
 @app.route('/')
 def hello():
-	out = '''<html><body style="font-size: 3em; margin: 100px; text-align:center">
-	<p>Hi. You\'ve reached the server side of <a href="http://opentaba.info">opentaba.info</a></p>
-	<p><a href="https://github.com/niryariv/opentaba-server">Source</a></p>
+    out = '''<html><body style="font-size: 3em; margin: 100px;
+    text-align:center">
+    <p>Hi. You\'ve reached the server side of
+    <a href="http://opentaba.info">opentaba.info</a></p>
+    <p><a href="https://github.com/niryariv/opentaba-server">Source</a></p>
 </body></html>'''
 
-	return out
+    return out
 
 
-# wake up heroku dyno from idle. perhaps can if >1 dynos 
+# wake up heroku dyno from idle. perhaps can if >1 dynos
 # used as endpoint for a "wakeup" request when the client inits
 @app.route('/wakeup')
 def wakeup():
-	return _resp({'morning' : 'good'})
+    return _resp({'morning': 'good'})
 
 
 #### MAIN ####
