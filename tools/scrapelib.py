6a75c65cc71a7bbf4ba3ec77fa3c4079d6b30223
Nir Yariv <niryariv@gmail.com>
2014-05-09 19:14:30 +0300

    fix scraping URLs / parser to match RMI latest moronic changes

--- tools/scrapelib.py
+++ tools/scrapelib.py
@@ -16,7 +16,12 @@ log = logging.getLogger(__name__)
 
 
 def get_gush_json_page(requests_session, page_num, cookie, view_state, data_source):
-    r = requests_session.post('http://mmi.gov.il/IturTabot2/taba1.aspx', cookies=cookie, data={'scriptManagerId_HiddenField':None,'__EVENTTARGET':None,'__EVENTARGUMENT':None,'__VIEWSTATE':view_state,'cpe_ClientState':None,'txtMsTochnit':None,'cmsStatusim$textBox':None,'txtGush':None,'txtwinCal1$textBox':None,'txtwinCal1$popupWin$time':None,'txtwinCal1$popupWin$mskTime_ClientState':None,'txtFromHelka':None,'txtwinCal2$textBox':None,'txtwinCal2$popupWin$time':None,'txtwinCal2$popupWin$mskTime_ClientState':None,'txtMakom':None,'cmsMerchaveiTichnun$textBox':None,'cmsYeudRashi$textBox':None,'txtMatara':None,'cmsYeshuvim$textBox':None,'cmsKodAchrai$textBox':None,'cmsTakanon$textBox':None,'txtAchrai':None,'cmsSug$textBox':None,'cmsMmg$textBox':None,'cmsKodMetachnen$textBox':None,'cmsTasrit$textBox':None,'txtMetachnen':None,'__CALLBACKID':'scriptManagerId','__CALLBACKPARAM':'Mmi.Tashtiot.UI.AjaxComponent.TableView$#$~$#$GetData$#${"P0":"'+data_source+'","P1":'+str(page_num)+',"P2":-1,"P3":"~","P4":"~","P5":"~","P6":true,"P7":true}'})
+    r = requests_session.post(
+        'http://mmi.gov.il/IturTabot2/taba1.aspx', 
+        cookies=cookie, 
+        data={'scriptManagerId_HiddenField':None,'__EVENTTARGET':None,'__EVENTARGUMENT':None,'__VIEWSTATE':view_state,'cpe_ClientState':None,'txtMsTochnit':None,'cmsStatusim$textBox':None,'txtGush':None,'txtwinCal1$textBox':None,'txtwinCal1$popupWin$time':None,'txtwinCal1$popupWin$mskTime_ClientState':None,'txtFromHelka':None,'txtwinCal2$textBox':None,'txtwinCal2$popupWin$time':None,'txtwinCal2$popupWin$mskTime_ClientState':None,'txtMakom':None,'cmsMerchaveiTichnun$textBox':None,'cmsYeudRashi$textBox':None,'txtMatara':None,'cmsYeshuvim$textBox':None,'cmsKodAchrai$textBox':None,'cmsTakanon$textBox':None,'txtAchrai':None,'cmsSug$textBox':None,'cmsMmg$textBox':None,'cmsKodMetachnen$textBox':None,'cmsTasrit$textBox':None,'txtMetachnen':None,'__CALLBACKID':'scriptManagerId',
+            '__CALLBACKPARAM':'Mmi.Tashtiot.UI.AjaxComponent.TableView$#$~$#$GetData$#${"P0":"'+data_source+'","P1":'+str(page_num)+',"P2":-1,"P3":["mtysvShemYishuv","Link","Status","tbMahut","Takanon","Tasrit","Nispach","Mmg"],"P4":"~","P5":"~","P6":true,"P7":true}'
+        })
     return r.text
 
 
@@ -32,28 +37,69 @@ def get_gush_json(gush_id):
         # Get the base search page and save the aspx session cookie, data source and view state
         r = ses.get('http://mmi.gov.il/IturTabot2/taba1.aspx')
         yum = r.cookies
+        # print 'GET http://mmi.gov.il/IturTabot2/taba1.aspx', r.text
 
         data_source = re.findall(r'tblView_[A-Za-z0-9]+', r.text)[-1]
 
         html = BeautifulSoup(r.text, 'lxml', from_encoding=SITE_ENCODING)
         view_state = html('input', id='__VIEWSTATE')[0]['value']
-    
+
         # Tell the server which fields we are displaying
-        r = ses.post('http://mmi.gov.il/IturTabot2/taba1.aspx', cookies=yum, data={'scriptManagerId_HiddenField':None,'__EVENTTARGET':None,'__EVENTARGUMENT':None,'__VIEWSTATE':view_state,'cpe_ClientState':None,'txtMsTochnit':None,'cmsStatusim$textBox':None,'txtGush':None,'txtwinCal1$textBox':None,'txtwinCal1$popupWin$time':None,'txtwinCal1$popupWin$mskTime_ClientState':None,'txtFromHelka':None,'txtwinCal2$textBox':None,'txtwinCal2$popupWin$time':None,'txtwinCal2$popupWin$mskTime_ClientState':None,'txtMakom':None,'cmsMerchaveiTichnun$textBox':None,'cmsYeudRashi$textBox':None,'txtMatara':None,'cmsYeshuvim$textBox':None,'cmsKodAchrai$textBox':None,'cmsTakanon$textBox':None,'txtAchrai':None,'cmsSug$textBox':None,'cmsMmg$textBox':None,'cmsKodMetachnen$textBox':None,'cmsTasrit$textBox':None,'txtMetachnen':None,'__CALLBACKID':'scriptManagerId','__CALLBACKPARAM':'Mmi.Tashtiot.UI.AjaxComponent.TableView$#$~$#$SetColumnList$#${"P0":"'+data_source+'","P1":"mtysvShemYishuv,Link,Status,tbMahut,Takanon,Tasrit,Nispach,Mmg,tbMsTochnit"}'})
+        r = ses.post('http://mmi.gov.il/IturTabot2/taba1.aspx', cookies=yum, data={
+            'scriptManagerId_HiddenField':None,
+            '__EVENTTARGET':None,
+            '__EVENTARGUMENT':None,
+            '__VIEWSTATE':view_state,
+            'cpe_ClientState':None,
+            'txtMsTochnit':None,
+            'cmsStatusim$textBox':None,
+            'txtGush':None,
+            'txtwinCal1$textBox':None,
+            'txtwinCal1$popupWin$time':None,
+            'txtwinCal1$popupWin$mskTime_ClientState':None,
+            'txtFromHelka':None,
+            'txtwinCal2$textBox':None,
+            'txtwinCal2$popupWin$time':None,
+            'txtwinCal2$popupWin$mskTime_ClientState':None,
+            'txtMakom':None,
+            'cmsMerchaveiTichnun$textBox':None,
+            'cmsYeudRashi$textBox':None,
+            'txtMatara':None,
+            'cmsYeshuvim$textBox':None,
+            'cmsKodAchrai$textBox':None,
+            'cmsTakanon$textBox':None,
+            'txtAchrai':None,
+            'cmsSug$textBox':None,
+            'cmsMmg$textBox':None,
+            'cmsKodMetachnen$textBox':None,
+            'cmsTasrit$textBox':None,
+            'txtMetachnen':None,
+            '__CALLBACKID':'scriptManagerId',
+            '__CALLBACKPARAM': 'Mmi.Tashtiot.UI.AjaxComponent.TableView$#$~$#$GetData$#${"P0":"'+data_source+'","P1":0,"P2":-1,"P3":["mtysvShemYishuv","Link","Status","tbMahut","Takanon","Tasrit","Nispach","Mmg"],"P4":"~","P5":"~","P6":true,"P7":true}'
+            })
+        # print 'POST http://mmi.gov.il/IturTabot2/taba1.aspx', r.text
 
         # Send a parameterized request to the server (just search for the gush)
-        r = ses.post('http://mmi.gov.il/IturTabot2/taba1.aspx/getNetuneiTochniotByAllParames', headers={'Content-Type':'application/json'}, cookies=yum, data=json.dumps({'SourceName':data_source,'conMachoz':0,'IsOneRow':False,'rtncol':2,'sKodMetachnen':'~','sKodAchrai':'~','iMachoz':'-1','sKodIshuv':'~','sMisTochnit':'~','sMatara':'~','sMakom':'~','sSug':'~','sVaada':'~','sYeudRashi':'~','sAchrai':'~','sMetachnen':'~','iMaamadMoncipali':'-1','iGush':gush_id,'iFromHelka':'-1','iToHelka':'-1','sTik':'~','sMerchav':'~','sKvutzatStatusim':'~','sFromTaarichStatus':'~','sToTaarichStatus':'~','bBProjects':False,'iNumOfRows':300,'sMmg':'~','sTasrit':'~','sTabaSruka':'~','sTakanon':'~'}))
+        r = ses.post(
+            'http://mmi.gov.il/IturTabot2/taba1.aspx/getNetuneiTochniotByAllParames', 
+            headers={'Content-Type':'application/json'}, 
+            cookies=yum, 
+            data=json.dumps({
+                'IsOneRow': False,'SourceName': data_source,'bBProjects': False,'conMachoz': 0,'iFromHelka': "-1",'iGush': gush_id,'iMaamadMoncipali': "-1",'iMachoz': "-1",'iNumOfRows': 300,'iToHelka': "-1",'rtncol': 2,'sAchrai': "~",'sFromTaarichStatus': "~",'sKodAchrai': "~",'sKodIshuv': "~",'sKodMetachnen': "~",'sKvutzatStatusim': "~",'sMakom': "~",'sMatara': "~",'sMerchav': "~",'sMetachnen': "~",'sMisTochnit': "~",'sMmg': "~",'sSug': "~",'sTabaSruka': "~",'sTakanon': "~",'sTasrit': "~",'sTik': "~",'sToTaarichStatus': "~",'sVaada': "~",'sYeudRashi': "~"
+          })
+        )
+        # print 'POST http://mmi.gov.il/IturTabot2/taba1.aspx/getNetuneiTochniotByAllParames', r.text
 
         result = []
         page = 0
-    
+
         # Get the first page of results and extra data
         first_page = get_gush_json_page(ses, page, yum, view_state, data_source)
         result = result + json.loads(re.findall('\[.*?\]', first_page)[0])
-    
+
         # Get the number of pages from the first page (every page has this)
         pages = int(re.findall('\$([\-#0-9]*)', re.findall('\](.*?){', first_page)[0])[7])
-    
+
         # Get the rest of the pages
         while page < pages:
             page = page + 1
@@ -72,7 +118,6 @@ def extract_data(gush_json):
     data = []
     
     for plan in gush_json:
-
         rec = {"area": '',
                "number": '',
                "details_link": '',
@@ -87,10 +132,10 @@ def extract_data(gush_json):
                "govmap_link": []}
 
         rec["area"] = plan["mtysvShemYishuv"].strip()
-        rec["number"] = plan["tbMsTochnit"].strip()
         
         bs = BeautifulSoup(plan["Link"], "lxml")
         rec["details_link"] = bs('a')[0].get('href').replace("javascript:openDetailesPage('", 'http://mmi.gov.il/IturTabot2/').replace("','", '', 1).replace("','", '&tbMsTochnit=').replace("')", '')
+        rec["number"] = bs('a')[0].contents[0]
 
         rec["status"] = plan["Status"].strip()
 
@@ -133,7 +178,9 @@ def extract_data(gush_json):
                     rec["govmap_link"].append(url)
 
         data.append(rec)
+        
 
+    # print "DATA:", data
     return data
 
 
@@ -181,6 +228,7 @@ def scrape_gush(gush, RUN_FOLDER=False):
 
     # check if the html matches a pre-read html
     # html_hash = md5.new(html.encode('utf-8')).hexdigest()
+
     if gush["html_hash"] == json_hash:
         log.debug("Gush data is not modified, returning")
         return True
