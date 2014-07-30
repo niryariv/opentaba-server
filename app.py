1410f8917bb43a297ba51d0b5926c8ee2887ab68
florpor <florist.powers@gmail.com>
2014-07-30 12:19:44 +0300

    now using a pylibmc client directly for SASL support

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ from werkzeug.urls import url_encode
 from flask import Flask
 from flask import abort, make_response, request
 
+from pylibmc import Client
+
 app = Flask(__name__)
 
 MONGO_URL = os.environ.get('MONGOHQ_URL')
@@ -37,66 +39,12 @@ else:  # work locally
     RUNNING_LOCAL = True
     app.debug = True  # since we're local, keep debug on
 
+# cache connection retry data
+MAX_CACHE_RETRIES = 3
+app.cache_retry = 0
+
 
 #### Helpers ####
-
-
-def cached(timeout=3600, cache_key=None, set_expires=True):
-    """
-    Caching decorator for Flask routes
-
-    Provides both caching and setting of relevant "Expires" header if appropriate.
-    Adapted from http://flask.pocoo.org/docs/patterns/viewdecorators/
-    """
-    def decorator(f):
-        @wraps(f)
-        def decorated_function(*args, **kwargs):
-            if cache_key is None:
-                ck = 'view:%s?%s' % (request.path, request.query_string)
-            else:
-                ck = cache_key
-            ek = '%s.expires' % ck
-            expires = None
-            cached = app.cache.get_many(ck, ek)
-            if cached[0] is not None:
-                response = cached[0]
-                app.logger.debug('Cache hit for %s, returning cached content, expires=%d', ck, cached[1])
-                if cached[1] is not None and set_expires:
-                    expires = cached[1]
-            else:
-                response = f(*args, **kwargs)
-                expires = int(time() + timeout)
-                app.cache.set_many({ck: response, ek: expires}, timeout=timeout)
-                app.logger.debug('Cache miss for %s, refreshed content and saved in cache, expires=%d', ck, expires)
-
-            if set_expires and expires is not None:
-                response.headers['Expires'] = email.utils.formatdate(expires)
-
-            return response
-
-        return decorated_function
-
-    return decorator
-
-
-@app.before_first_request
-def _setup_cache():
-    """
-    We only initialize the cache here because __main__ is not run when we launch within a WSGI container.
-    If a test is being run or we don't want cache, NullCache will be initialized just as a dummy.
-    If running locally without the 'DISABLE_CACHE' env variable and without a memcached instance running,
-    MemcachedCache and it's underlying python-memcached will give no warning, and simply behave like a
-    NullCache. Some monitoring should be done for the degug cache hit/miss messages.
-    """
-    # Setup cache
-    if app.config['TESTING'] or os.environ.get('DISABLE_CACHE', None) is not None:
-        app.cache = NullCache()
-        app.logger.debug('Cache initialized as NullCache')
-    else:
-        MEMCACHED_SERVERS = os.environ.get('MEMCACHEDCLOUD_SERVERS', '127.0.0.1:11211')
-        app.cache = MemcachedCache(servers=MEMCACHED_SERVERS.split(';'))
-        app.logger.debug("Cache initialized as MemcachedCache with servers: %s", MEMCACHED_SERVERS)
-
 
 def _to_json(mongo_obj):
     """
@@ -160,6 +108,102 @@ def _plans_query_to_atom_feed(request, query={}, limit=0, feed_title=''):
         )
 
     return feed
+
+
+#### Cache ####
+
+@app.before_first_request
+def setup_cache():
+    """
+    We initialize the cache here for the first time because __main__ is not run when we launch
+    within a WSGI container.
+    """
+    _setup_cache()
+
+
+def _setup_cache():
+    """
+    If a test is being run or we don't want cache, NullCache will be initialized just as a dummy.
+    If running locally without the 'DISABLE_CACHE' env variable and without a memcached instance running,
+    MemcachedCache and it's underlying pylibmc will give no warning on connection, but will throw
+    exceptions when trying to work with the cache. A few connection retires will be made in that
+    scenario, and eventually the cache will be replaced with a NullCache. Binary communications must
+    be used for SASL.
+    """
+    # Setup cache
+    if app.config['TESTING'] or os.environ.get('DISABLE_CACHE', None) is not None:
+        app.cache = NullCache()
+        app.logger.debug('Cache initialized as NullCache')
+    else:
+        MEMCACHED_SERVERS = os.environ.get('MEMCACHEDCLOUD_SERVERS', '127.0.0.1:11211')
+        
+        try:
+            memcached_client = Client(servers=MEMCACHED_SERVERS.split(','),
+                                      username=os.environ.get('MEMCACHEDCLOUD_USERNAME'),
+                                      password=os.environ.get('MEMCACHEDCLOUD_PASSWORD'),
+                                      binary=True)
+            app.cache = MemcachedCache(memcached_client)
+            app.logger.debug('Cache initialized as MemcachedCache with servers: %s', MEMCACHED_SERVERS)
+        except Exception as e:
+            # very unlikely to have an exception here. pylibmc mostly throws when trying to communicate, not connect
+            app.logger.error('Error initializing MemcachedCache: %s', e);
+            app.logger.error('Initializing cache as NullCache. Fix ASAP!');
+            app.cache = NullCache()
+
+
+def cached(timeout=3600, cache_key=None, set_expires=True):
+    """
+    Caching decorator for Flask routes
+
+    Provides both caching and setting of relevant "Expires" header if appropriate.
+    Adapted from http://flask.pocoo.org/docs/patterns/viewdecorators/
+    """
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if cache_key is None:
+                ck = 'view:%s?%s' % (request.path, request.query_string)
+            else:
+                ck = cache_key
+            ek = '%s.expires' % ck
+            response = None
+            expires = None
+            
+            # pylibmc will throw an error when trying to communicate with memcached, not upon a bad connection
+            try:
+                cached = app.cache.get_many(ck, ek)
+                if cached[0] is not None:
+                    response = cached[0]
+                    app.logger.debug('Cache hit for %s, returning cached content, expires=%d', ck, cached[1])
+                    if cached[1] is not None and set_expires:
+                        expires = cached[1]
+                else:
+                    response = f(*args, **kwargs)
+                    expires = int(time() + timeout)
+                    app.cache.set_many({ck: response, ek: expires}, timeout=timeout)
+                    app.logger.debug('Cache miss for %s, refreshed content and saved in cache, expires=%d', ck, expires)
+
+                if set_expires and expires is not None:
+                    response.headers['Expires'] = email.utils.formatdate(expires)
+            except Exception as e:
+                app.logger.error('Cache error, returning miss: %s', e)
+                if response is None:
+                    response = f(*args, **kwargs)
+                
+                if (type(app.cache) is not NullCache):
+                    if (app.cache_retry < MAX_CACHE_RETRIES):
+                        app.cache_retry += 1
+                        app.logger.error('Attempting to restore cache')
+                        _setup_cache()
+                    else:
+                        app.logger.error('Exhausted retry attempts. Converting cache to NullCache. Fix ASAP!')
+                        app.cache = NullCache()
+
+            return response
+
+        return decorated_function
+
+    return decorator
 
 
 #### ROUTES ####
