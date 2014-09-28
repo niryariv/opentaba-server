9acbfe4a2274c4c0cff404dfd5e9679caae8b41e
florpor <florist.powers@gmail.com>
2014-09-28 14:30:53 +0300

    revert #6fd3cda

--- Tests/functional_tests/test_return_json.py
+++ Tests/functional_tests/test_return_json.py
@@ -31,7 +31,7 @@ def test_api_gushim():
     response = testapp.get('/gushim.json')
     print(dir(response))
     j = json.loads(response.data)
-    eq_(len(j), 1450)  # the correct number
+    eq_(len(j), 1196)  # the correct number
     eq_(response.status_code, 200)
     eq_(response.mimetype, 'application/json')
 
