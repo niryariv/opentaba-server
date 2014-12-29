db6af05cd2d52f17455be9f39924d2edb4824bd1
florpor <florist.powers@gmail.com>
2014-12-29 10:54:43 +0200

    moved social token storage to poster service

--- tools/sociallib.py
+++ tools/sociallib.py
@@ -11,21 +11,10 @@ log = logging.getLogger(__name__)
 
 
 def post(plan):
-    if 'SOCIAL_SERVICE_URL' in os.environ.keys():
-        # generate a formatted plan
-        post_data = {'plan': dumps(helpers._format_plan(plan), cls=helpers.DateTimeEncoder)}
-        
-        # if we have facebook posting settings, add them
-        if 'FB_TOKEN' in os.environ.keys() and 'FB_PAGE_ID' in os.environ.keys():
-            post_data['fb_tok'] = os.environ['FB_TOKEN']
-            post_data['fb_page'] = os.environ['FB_PAGE_ID']
-        
-        # same for twitter settings
-        if 'TW_TOKEN' in os.environ.keys() and 'TW_TOKEN_SECRET' in os.environ.keys() and 'TW_CONSUMER' in os.environ.keys() and 'TW_CONSUMER_SECRET' in os.environ.keys():
-            post_data['tw_tok'] = os.environ['TW_TOKEN']
-            post_data['tw_tsec'] = os.environ['TW_TOKEN_SECRET']
-            post_data['tw_con'] = os.environ['TW_CONSUMER']
-            post_data['tw_csec'] = os.environ['TW_CONSUMER_SECRET']
+    if all(param in os.environ.keys() for param in ['POSTER_SERVICE_URL', 'POSTER_ID']):
+        # generate a formatted plan and the post data
+        formatted_plan = helpers._format_plan(plan)
+        post_data = {'poster_id': os.environ['POSTER_ID'], 'title': formatted_plan['title'], 'content': formatted_plan['content'], 'url': formatted_plan['url']}
         
         # send data to social poster service. we just get an ok and continue, it's up to the service to take care of errors and such
-        requests.post(os.environ['SOCIAL_SERVICE_URL'], data=post_data)
+        requests.post(os.environ['POSTER_SERVICE_URL'], data=post_data)
