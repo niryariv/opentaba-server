d744b48b1761656c84f262354e35748773e3d00d
florpor <florist.powers@gmail.com>
2015-09-16 18:06:04 +0300

    fix compatibility with new pymongo version

--- lib/conn.py
+++ lib/conn.py
@@ -12,12 +12,12 @@ MONGO_URL = os.environ.get('MONGOLAB_URI') or os.environ.get('MONGOHQ_URL')
 
 
 if MONGO_URL:  # on Heroku, get a connection
-    m_conn = pymongo.Connection(MONGO_URL)
+    m_conn = pymongo.MongoClient(MONGO_URL)
     db = m_conn[urlparse(MONGO_URL).path[1:]]
     RUNNING_LOCAL = False
 else:  # work locally
     try:
-        m_conn = pymongo.Connection('localhost', 27017)
+        m_conn = pymongo.MongoClient('localhost', 27017)
     except ConnectionFailure:
         print('You should have mongodb running')
 
